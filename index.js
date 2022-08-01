796ecc56f5ec88f4791f0778e03e2e7fe564713d
iamAbayomi <oladinifolawiyo@gmail.com>
2022-08-01 14:59:52 +0100

    feat: tested additional arrays

--- index.js
+++ index.js
@@ -8,6 +8,21 @@ queue.enqueue(1)
 queue.enqueue(2)
 queue.enqueue(3)
 queue.enqueue(4)
-queue.dequeue()
+//queue.dequeue()
 
 console.log("Stack", stack, "Queue", queue)
+
+const reversedQueue = []
+for(let i=0; i<=queue.length; i++){
+    reversedQueue.push(queue.dequeue())   
+}
+
+
+console.log("reversed Queue", reversedQueue.reverse())
+console.log("values", reversedQueue.values())
+
+const values = reversedQueue.values()
+
+for(const value of values){
+    console.log("value", value)
+}
